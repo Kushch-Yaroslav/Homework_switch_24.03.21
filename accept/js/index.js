0b04f01c072df6e40cbac72e789f92a6c1a87c9e
Kushch-Yaroslav <malborodo123@gmail.com>
2021-03-24 22:54:40 +0200

    master

--- accept/js/index.js
+++ accept/js/index.js
@@ -1,55 +1,12 @@
-// const userInputDay = +prompt("Введите число: ", );
-// switch(userInputDay){
-//   case 1:
-//   case 2:
-//   case 3:
-//   case 4:
-//   case 5:
-//   case 6:
-//   case 7:
-//   case 8:
-//   case 9:
-//   case 10:{
-//     console.log("Это первая декада.")
-//     break;
-//   }
-//    case 11:
-//   case 12:
-//   case 13:
-//   case 14:
-//   case 15:
-//   case 16:
-//   case 17:
-//   case 18:
-//   case 19:
-//   case 20:{
-//    console.log("Это вторая декада.")
-//    break;
-//     }
-//   case 21:
-//   case 22:
-//   case 23:
-//   case 24:
-//   case 25:
-//   case 26:
-//   case 27:
-//   case 28:
-//   case 29:
-//   case 30:{
-//         console.log("Это это третья декада.")
-//         break;
-//   }
-//   default:{
-//     console.log("В месяце максимум 31 день!")
-//   }
-// }
+
 
 
 const calculation = +prompt(
-  'Выберите математическую операцию ! \n 1 - Сложение(+) \n 2 - Вычитание(-) \n 3 - Умножение(*) \n 4 - Деление(/) \n 5 - Возведение в степень(**) \n 6 - Возвращает квадратный корень числа (Вводите только первое число!)',
+  'Выберите математическую операцию ! \n 1 - Сложение(+) \n 2 - Вычитание(-) \n 3 - Умножение(*) \n 4 - Деление(/) \n 5 - Возведение в степень(**) \n 6 - Возвращает квадратный корень числа (Вводите только первое число!) ' ,
 );
 const Number_1 = +prompt("Введите первое число: ", );
 const Number_2 = +prompt("Введите второе число: ", );
+const userInputDay = +prompt("Введите день: ", );
 
 
 const fold = Number_1 + Number_2;
@@ -84,4 +41,50 @@ case 6: {
   console.log("Квадратный корень из числа", Number_1, "равен", sq);
   break;
 }
+}
+
+
+switch(userInputDay){
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:{
+    console.log("Это первая декада.")
+    break;
+  }
+   case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  case 20:{
+   console.log("Это вторая декада.")
+   break;
+    }
+  case 21:
+  case 22:
+  case 23:
+  case 24:
+  case 25:
+  case 26:
+  case 27:
+  case 28:
+  case 29:
+  case 30:{
+        console.log("Это это третья декада.")
+        break;
+  }
+  default:{
+    console.log("В месяце максимум 31 день!")
+  }
 }
